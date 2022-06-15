a228e9b368e183aa051dc136ffc5d9634d5e21c3
KeeganHeitman <74746845+KeeganHeitman@users.noreply.github.com>
2022-06-15 14:42:26 -0700

    Add files via upload

---
+++ hypergrade2_1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class hypergrade2_1
+{
+    public static void main(String[] args)
+    {
+        Scanner keyboard = new Scanner(System.in);
+        
+        System.out.println("Enter number");
+        int number1 = keyboard.nextInt();
+        
+        System.out.println(number1);
+        
+        int answer=number1+1;
+        
+        System.out.println(answer);
+    }
+}
